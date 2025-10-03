ff880c0f61338d0d176f937c13d163f6cc4b50d0
Pritam Rauniyar <pritamrauniyar.np@gmail.com>
2025-10-03 11:47:07 +0530

    Added design and remove the device configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { DeviceDetectorService } from 'ngx-device-detector';
-import { SpeedTestService } from 'ng-speed-test';
+import { Component } from '@angular/core';
 import { TranscriberService } from './services/transcriber.service';
 
 @Component({
@@ -8,8 +6,7 @@ import { TranscriberService } from './services/transcriber.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  title = 'speed-and-device-details';
+export class AppComponent {
   
   deviceInfo: any;
   orientation: string = '';
@@ -17,74 +14,8 @@ export class AppComponent implements OnInit {
   uploadSpeed: string | null = null;
 
   constructor(
-    private deviceService: DeviceDetectorService,
-    private speedTestService: SpeedTestService,
     public transcriber: TranscriberService
   ) {}
 
-  ngOnInit() {
-    //uncomment this
-    // Get device details
-    // this.deviceInfo = this.deviceService.getDeviceInfo();
-    // console.log(this.deviceInfo);
-    // this.orientation = window.innerWidth > window.innerHeight ? 'landscape' : 'portrait';
-
-    // // Get download speed with error handling
-    // this.speedTestService.getMbps().subscribe({ 
-    //   next: (speed: number) => {
-    //     this.downloadSpeed = speed.toFixed(2);
-    //   },
-    //   error: (err: any) => {
-    //     console.error('Error fetching download speed:', err);
-    //     this.downloadSpeed = 'Error';
-    //   }
-    // });
-
-    // Get upload speed with error handling
-    // this.speedTestService.getUploadMbps$().subscribe({ // Correct method name might be `getUploadMbps$()`
-    //   next: (speed: number) => {
-    //     this.uploadSpeed = speed.toFixed(2);
-    //   },
-    //   error: (err: any) => {  // Added typing for `err`
-    //     console.error('Error fetching upload speed:', err);
-    //     this.uploadSpeed = 'Error';
-    //   }
-    // });
-    //uncomment this
-    // this.speedTestService.isOnline().subscribe(
-    //   (isOnline) => {
-    //     if (isOnline === false) {
-    //       console.log('Network unavailable.');
-    //     }
-    //   }
-    // );
-  }
-
-  isDesktop(): boolean {
-    return this.deviceService.isDesktop();
-  }
-
-  isMobile(): boolean {
-    return this.deviceService.isMobile();
-  }
-
-  isTablet(): boolean {
-    return this.deviceService.isTablet();
-  }
-  // src/app/app.component.ts
-  isModalOpen = false;
-
-  openModal() {
-    this.isModalOpen = true;
-  }
-
-  closeModal() {
-    this.isModalOpen = false;
-  }
-
-  submitModal() {
-    console.log('Modal submitted!');
-    this.isModalOpen = false;
-  }
 }
 
